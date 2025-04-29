5e7f60dabcc39deb81ad760fcaf03e42f12f1e28
rayssa0433 <rayssa0433@gmail.com>
2025-04-29 13:26:44 +0000

    Arrow function

--- main.js
+++ main.js
@@ -9,7 +9,26 @@
   } 
   let somar = function (valor1, valor2) { 
   return valor1 + valor2 
-  } 
+ } 
   console.log(mensagem()) 
   console.log(somar (10, 20)) 
+ */
+  
+  
+  
+  // arrow function 
+  
+  
+  /*
+  let mensagem = () => { 
+  console.log("Olá, tudo bem?") 
+  } 
+  
+  let somar = (valor1, valor2) => valor1 + valor2 
+  
+  let raizQuadrada = (valor1) => Math.sqrt(valor1) 
+  
+  console.log(mensagem()) 
+  console.log(somar(25, 768)) 
+  console.log(raizQuadrada (49))
   */
