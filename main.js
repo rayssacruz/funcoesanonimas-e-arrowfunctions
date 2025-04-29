a9bf5f894c28ff05a65579ea490958894577f20b
rayssa0433 <rayssa0433@gmail.com>
2025-04-29 13:25:53 +0000

    Função anonima

---
+++ main.js
@@ -0,0 +1,15 @@
+// funções anônimas = funções sem nome 
+  
+  // função anônima comum / antiga 
+  
+  
+  /* 
+  let mensagem = function() { 
+  console.log("Bem-vindo(a)!") 
+  } 
+  let somar = function (valor1, valor2) { 
+  return valor1 + valor2 
+  } 
+  console.log(mensagem()) 
+  console.log(somar (10, 20)) 
+  */
